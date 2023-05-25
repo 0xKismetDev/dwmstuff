54eb87f64ba411c676b4760999822895e5505872
KismetDev <131729061+0xKismetDev@users.noreply.github.com>
2023-05-25 17:56:54 +0200

    main stuff

---
+++ README.md
@@ -0,0 +1,12 @@
+# dwmstuff
+dwm &amp; dwmblocks config and binaries + custom scripts &amp; dotfiles
+
+Some files taken/edited from [Luke Smith](https://github.com/LukeSmithxyz/voidrice.git)
+
+![vmware_4mno3pWUGq](https://user-images.githubusercontent.com/75517494/185510357-c83ce8af-b12b-4145-86db-b7248d48b67e.png)
+
+# Custom Scripts
+
+- /scripts/setbg
+
+will set specified image as background and use pywal to change colorscheme
---
+++ dwmblocks/blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"[  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g ",	15,		0},
+	{"[  ", "~/dwmblocks/scripts/cpu",							15, 		0},
+        {"[  ", "~/dwmblocks/scripts/updates",                                     	120,            0},
+	{"[  ", "~/dwmblocks/scripts/user",                                        	300,            0},
+	{"","echo [$(date '+ %H:%M - %A, %B %d') ]",				5,		0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ] ";
+static unsigned int delimLen = 5;
